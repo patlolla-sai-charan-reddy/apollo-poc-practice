edede2e56fd47a6b93d2ed145cccb2684c0a6deb
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2019-04-10 17:06:23 -0400

    pratcice

--- src/index.js
+++ src/index.js
@@ -2,11 +2,74 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider, Query } from "react-apollo";
+import gql from "graphql-tag";
 
 const client = new ApolloClient({
   uri: `https://nx9zvp49q7.lp.gql.zone/graphql`
 });
+
+const GET_DOGS = gql`
+  {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
+
+const Dogs = ({ onDogSelected }) => (
+  <Query query={GET_DOGS}>
+    {({ loading, error, data }) => {
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <select name="dog" onChange={onDogSelected}>
+          {data.dogs.map(dog => (
+            <option key={dog.id} value={dog.breed}>
+              {dog.breed}
+            </option>
+          ))}
+        </select>
+      );
+    }}
+  </Query>
+);
+
+const GET_DOG_PHOTO = gql`
+  query dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+const DogPhoto = ({ breed }) => (
+  <Query
+    query={GET_DOG_PHOTO}
+    variables={{ breed }}
+    notifyOnNetworkStatusChange
+    // pollInterval={500}
+  >
+    {({ loading, error, data, refetch, networkStatus }) => {
+      if (networkStatus === 4) return "Refetching!";
+      if (loading) return null;
+      if (error) return `Error!: ${error}`;
+
+      return (
+        <div>
+          <img
+            src={data.dog.displayImage}
+            style={{ height: 100, width: 100 }}
+          />
+          <button onClick={() => refetch()}>Refetch!</button>
+        </div>
+      );
+    }}
+  </Query>
+);
 
 class App extends Component {
   state = { selectedDog: null };
@@ -20,6 +83,10 @@ class App extends Component {
       <ApolloProvider client={client}>
         <div>
           <h2>Building Query components 🚀</h2>
+          {this.state.selectedDog && (
+            <DogPhoto breed={this.state.selectedDog} />
+          )}
+          <Dogs onDogSelected={this.onDogSelected} />
         </div>
       </ApolloProvider>
     );
